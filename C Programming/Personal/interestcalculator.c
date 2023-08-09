47e019789ab508bc43803ff03e923558f0de827c
Akhil Mahesh <124284817+RhythmusByte@users.noreply.github.com>
2023-08-09 15:46:39 +0530

    Create interestcalculator.c

---
+++ C Programming/Personal/interestcalculator.c
@@ -0,0 +1,39 @@
+/* Investment Problem */
+
+/* Defining values of PERIOD & PRINCIPAL */
+
+#define PERIOD 2010
+#define PRINCIPAL 5000.00
+
+main()
+{
+
+  /* Declaration Statement*/ 
+  /* Declared variables */
+  
+    int year;
+    float amount,value,inrate;
+
+/* Assignment Statements */
+/* Assigned values for variables */
+  
+    amount = PRINCIPAL;
+    inrate = 0.11;
+    year = 2000;
+
+  /* Computation Statement */
+  /* Using While Loop */
+  
+    while(year <= PERIOD)
+    {
+    	printf("%2d  %8.2f\n",year,amount);
+        
+        value = amount + inrate * amount;
+        year = year + 1;
+        amount = value;
+        
+    }  
+
+  /* While Loop Ends*/
+  
+}    
