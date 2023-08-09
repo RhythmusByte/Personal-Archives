4ad74a5bdf5bab071be3556600b68fb8eef3e7c7
Akhil Mahesh <124284817+RhythmusByte@users.noreply.github.com>
2023-08-09 16:34:13 +0530

    Create interestcalculator2.c

---
+++ C Programming/Personal/interestcalculator2.c
@@ -0,0 +1,53 @@
+/* Date - 09/08/2023 (DD/MM/YYYY) */
+/* Time - 16:33 (Indian Time) */
+/* Author - RhythmusByte */
+
+/* Header File */
+
+#include <stdio.h>
+
+/* Defining values */
+
+#define time 2010
+#define cash 5000.00
+#define per 0.11
+
+main()
+{
+
+/* Declaration Statements & Assigned Values */
+    
+	int year=2000;
+    float amount=cash, interest=per, value;
+    
+    /* while Loop Starts */
+    
+    while(year<=time)
+    {
+    	printf("Year: %d  Amount: %8.3f \n",year,amount);
+        	
+            value = amount + interest * amount;
+            year = year + 1;
+            amount = value;
+    }
+    
+    /* while Loop Ends */
+}
+
+/* Output */
+
+/*
+Year: 2000  Amount: 5000.000
+Year: 2001  Amount: 5550.000
+Year: 2002  Amount: 6160.500
+Year: 2003  Amount: 6838.155
+Year: 2004  Amount: 7590.352
+Year: 2005  Amount: 8425.290
+Year: 2006  Amount: 9352.072
+Year: 2007  Amount: 10380.800
+Year: 2008  Amount: 11522.688
+Year: 2009  Amount: 12790.184
+Year: 2010  Amount: 14197.104
+
+[Process completed - press Enter]
+*/
